54030776af4a8f22d06e9f8f9b0d48f06e3329bc
Apurba1903 <apurba1903@gmail.com>
2025-05-19 15:10:53 +0600

    pipeline in scrapy

---
+++ Scrapy/data_pipelines/data_pipelines/spiders/quotes_scraper.py
@@ -0,0 +1,20 @@
+import scrapy
+from data_pipelines.items import QuoteItem
+from data_pipelines.pipelines import QuotesPipeline
+
+
+class QuotesScraperSpider(scrapy.Spider):
+    name = "quotes_scraper"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/"]
+
+    def parse(self, response):
+        
+        for quote in response.css('div.quote'):
+            item  = QuoteItem()
+            item['text'] = quote.css('span.text::text').get()
+            item['author'] = quote.css('small.author::text').get()
+            item['tags'] = quote.css('div.tags a.tag::text').getall()
+            yield item
+
+        
